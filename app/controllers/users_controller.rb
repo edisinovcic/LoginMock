6cfb44c7ecd30b52405a9209604f29091e0abf88
Edi Sinovčić <edi.sinovcic@digacon-software.com>
2018-01-25 13:14:50 +0100

    Change in controller responses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,25 +7,31 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user
+      render json: user, status: 201
     else
-      render json: { message: 'User creation failed' }
+      render json: {message: 'User creation failed'}, status: 400
     end
   end
 
   def show
     user = User.find(params[:id])
-    render json: user
+    render json: user, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: {message: "User doesn't exists"}, status: 404
+
+
   end
 
   def update
-    user = user.find(params[:id])
+    user = User.find(params[:id])
     user.id = params[:id]
     if user.save
       render json: user
     else
-      render json: { message: 'Failed to create user' }
+      render json: {message: 'User creation failed'}, status: :unprocessable_entity
     end
+  rescue
+    render json: {message: 'User creation failed'}, status: :unprocessable_entity
   end
 
   def destroy
@@ -35,6 +41,8 @@ class UsersController < ApplicationController
     else
       render json: user.error, status: :unprocessable_entity
     end
+  rescue
+    render json: {message: 'User destroy failed'}, status: :unprocessable_entity
   end
 
   private
@@ -42,4 +50,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :username)
   end
+
 end
